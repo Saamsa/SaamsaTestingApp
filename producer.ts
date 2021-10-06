966b36514252ad056e303b1f07c273fd52d04ed4
Janilya <janilyabaizack@gmail.com>
2021-10-06 15:38:42 -0500

    Created producer. Set up schema. Pulled from faker

---
+++ producer.ts
@@ -0,0 +1,40 @@
+import { Kafka } from 'kafkajs';
+
+async function createProducer(producerName: string, message: object, topic: string) {
+  
+  // Configure the client to a seed broker
+  const kafka = new Kafka({
+    clientId: producerName,
+    brokers: ['kafka:9092'],
+  });
+
+  // Create an instance of a producer on kafka broker
+  const producer = kafka.producer();
+
+  // Connect to the producer
+  try {
+    await producer.connect();
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Write a message to producer
+  try {
+    await producer.send({
+      topic: topic,
+      messages: [{ value: JSON.stringify(message) }],  
+        //could also specify key [{ key: 'my-key', value: 'my-value'}],
+        //other options: partition, timestamp, headers
+      //acks: -1 all, 0 none, 1 just leader (default is -1)
+      //timeout:
+      //compression: 
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Disconnect from the producer
+  producer.disconnect();
+}
+
+export { createProducer };
---
+++ services/contactTracing.ts
@@ -0,0 +1,66 @@
+import faker from 'faker';
+
+/**
+ * @function createCaseReport creates a fake covid positive case report
+ * @returns an object with case information
+ */
+
+const createCaseReport = () => {
+  // Assign proper datatypes
+  interface covidPositive {
+    eventId: string,
+    eventDateTime: Date, 
+    eventName: string, 
+    firstName: string, 
+    lastName: string,
+    middleName: string,
+    gender: string,
+    street: string,
+    street2:string,
+    city: string,
+    county: string,
+    state: string,
+    zip: number,
+    latitude: number,
+    longitude: number, 
+    phone: number,
+    email: string,  
+    jobArea: string, 
+    jobTitle: string,
+    nameLastContact: string,
+    dateLastContact: Date,
+    countryLastTravel: string,
+    dateLastTravel: Date,
+  };
+  
+  // Declare an object with case information
+  const covidPositive = {
+    eventId: faker.datatype.uuid(),
+    eventTimstamp: faker.datatype.datetime(),
+    eventName: 'initial_report',
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    middleName: faker.name.middleName(),
+    gender: faker.name.gender(),
+    street: faker.address.streetAddress(),
+    street2:faker.address.secondaryAddress(),
+    city: faker.address.cityName(),
+    county: faker.address.county(),
+    state: faker.address.stateAbbr(),
+    zip: faker.address.zipCode(),
+    latitude: faker.address.latitude(),
+    longitude: faker.address.longitude(),
+    phone: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    jobArea: faker.name.jobArea(), 
+    jobTitle: faker.name.jobTitle(),
+    nameLastContact: faker.name.firstName(),
+    dateLastContact: faker.date.past(),
+    countryLastTravel: faker.address.country(), 
+    dateLastTravel: faker.date.past(),
+  };
+
+  return covidPositive;
+};
+
+export { createCaseReport };
