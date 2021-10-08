78dc004e2c67f3f9942b0c047c5dd26f48d4b888
Kasthuri Menon <kasthuri.menon1@gmail.com>
2021-10-07 21:36:57 -0400

    modified consumer file to remove errors

--- consumer.ts
+++ consumer.ts
@@ -1,35 +1,39 @@
-const { producer } = require('./producer'); 
-const { Kafka } = require ('kafkajs');
+import * as Kafka from 'kafkajs';
+
 /**
  * Creates a producer object for export later
- * @param groupId - the client ID for a new kafka instance
+ * @param groupId - required for consumer creation in Kafka
+ * @param topic - topic that matches producer
+ * 
  */
- const consumer = async (groupId: string) => {
-    // Instantiate new kafka object, creating two brokers on the same port
-    const kafka = new Kafka({
-      clientId: 'COVIDtransfer',
-      brokers: ['kafka:9092'],
-      // authenticationTimeout: 1000,
-      // reauthenticationThreshold: 10000,
-      ssl: true,
-      sasl: {
-      mechanism: 'aws',
-      authorizationIdentity: 'arn:aws:iam::852274558021:user/DynamoDBTemp', // UserId or RoleId
-      accessKeyId: 'AKIA4M34FWRC62OLOEN4',
-      secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-      sessionToken: 'WHArYt8i5vfQUrIU5ZbMLCbjcAiv/Eww6eL9tgQMJp6QFNEXAMPLETOKEN' // Optional
-  },
+ const createConsumer = async (groupId: string, topic: string) => {
+   
+  // Creating a new Kafka instance 
+  // Configure AWS IAM client with kafka broker
+   const kafka = new Kafka.Kafka({
+     clientId: groupId,
+     brokers: ['kafka:9092'],
+     // authenticationTimeout: 1000,
+     // reauthenticationThreshold: 10000,
+     ssl: true,
+     sasl: {
+       mechanism: 'aws',
+       authorizationIdentity: 'arn:aws:iam::852274558021:user/DynamoDBTemp', // UserId or RoleId
+       accessKeyId: 'AKIA4M34FWRC7OL2YPNK',
+       secretAccessKey: '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'
+      },
     });
-
-    const consumer = kafka.consumer({ groupId: groupId });
+    // Creating kafka consumer with required group ID
+ const consumer = kafka.consumer({ groupId: 'test-group' });
+//  kafka.consumer.groupId = {groupId}; 
 
 await consumer.connect()
-await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+await consumer.subscribe({ topic: topic, fromBeginning: true })
 
 
 await consumer.run({
   autoCommitInterval: 5000,
-  // eachMessage: sendEachMessage, -> to send to the DB 
+  // eachMessage: awsConnector.sendEachMessage, -> to send to the DB 
   eachMessage: async ({ topic, partition, message }) => {
     try{
       // listen for each message being sent, and then send the messages to the DB
@@ -48,30 +52,18 @@ await consumer.run({
   },
 });
 
-// add code to insert data into dynamoDB
-const sendEachMessage = async ({message}) => {
-// Declare a new variable set to the parsed JSON of message value
-const messageObj = await JSON.parse(message.value);
-console.log("The message recieved inside sendEachMessage is : ", messageObj);
-
-//connect with DynamoDB and send data to dynamoDB
-
-// add an error handler that consoles any error that occurs when you're inserting data to dynamoDB
-
-//console.log a success message if data is inserted
-
-//return
-}
-
 
 // Error Handler for DynamoDB data insertion
-function insertHandler(err) {
-  if (err) {
-    console.log(`Error caught in DynamoDB database insertion" ${err}`);
-    }
-  }
-}
+// function insertHandler(err) {
+//   if (err) {
+//     console.log(`Error caught in DynamoDB database insertion" ${err}`);
+//     }
+//   }
+};
 
- };
+ export { createConsumer };
 
- export { consumer };
+ createConsumer(
+  'CovidGroup1',
+  'initial_report'
+);
